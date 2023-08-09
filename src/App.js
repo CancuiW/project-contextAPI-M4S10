0243a64dc86f85a5c8390e8d6119c152efe76c4b
CancuiW <can.cui@wayne.edu>
2023-08-09 14:21:58 -0700

    finished

--- src/App.js
+++ src/App.js
@@ -7,27 +7,40 @@ import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
 
+import ProductContext from './contexts/ProductContext';
+import CartContext from './contexts/CartContext';
 function App() {
 	const [products] = useState(data);
 	const [cart, setCart] = useState([]);
 
 	const addItem = item => {
 		// add the given item to the cart
+		const newItem=[...cart,item]
+		setCart(newItem)
 	};
 
+	
+
 	return (
-		<div className="App">
-			<Navigation cart={cart} />
+		<ProductContext.Provider value={{ products, addItem }}>
+			<CartContext.Provider value={{cart,setCart}}>
 
-			{/* Routes */}
-			<Route exact path="/">
-				<Products products={products} addItem={addItem} />
-			</Route>
+				<div className="App">
+					<Navigation  />
 
-			<Route path="/cart">
-				<ShoppingCart cart={cart} />
-			</Route>
-		</div>
+					{/* Routes */}
+					<Route exact path="/">
+						<Products />
+					</Route>
+
+					<Route path="/cart">
+						<ShoppingCart  />
+					</Route>
+				</div>
+
+			</CartContext.Provider>
+		</ProductContext.Provider>
+		
 	);
 }
 
--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -1,24 +1,32 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
 // Components
 import Item from './ShoppingCartItem';
+import CartContext from '../contexts/CartContext';
 
-const ShoppingCart = props => {
+const ShoppingCart = () => {
+	const {cart,setCart}=useContext(CartContext)
 	const getCartTotal = () => {
-		return props.cart.reduce((acc, value) => {
+		return cart.reduce((acc, value) => {
 			return acc + value.price;
 		}, 0).toFixed(2);
 	};
+	const removeItem=()=>{
+		
+			setCart([])
+		
+	}
+
 
 	return (
 		<div className="shopping-cart">
-			{props.cart.map(item => (
+			{cart.map(item => (
 				<Item key={item.id} {...item} />
 			))}
 
 			<div className="shopping-cart__checkout">
 				<p>Total: ${getCartTotal()}</p>
-				<button>Checkout</button>
+				<button onClick={removeItem}>Checkout</button>
 			</div>
 		</div>
 	);
